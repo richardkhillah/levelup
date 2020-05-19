87b3f3cf4f27ad32d67a2e9410a30e1587dc3741
Richard Khillah <RKhillah@ucla.edu>
2020-05-18 22:53:44 -0700

    SQLAlchemy

--- hello.py
+++ hello.py
@@ -4,12 +4,22 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
 # manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
