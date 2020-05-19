64710bad561941d4d66b95868a3b4c669fe4aa6a
Richard Khillah <RKhillah@ucla.edu>
2020-05-18 21:14:08 -0700

    refactor templates and Moment

--- hello.py
+++ hello.py
@@ -1,14 +1,19 @@
+from datetime import datetime
+
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                            current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
@@ -21,6 +26,7 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     manager.run()
