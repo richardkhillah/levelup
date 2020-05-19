2e2a261e38124b68a826608fbf60dc29eb062edb
Richard Khillah <RKhillah@ucla.edu>
2020-05-18 22:09:35 -0700

    Name Form

--- hello.py
+++ hello.py
@@ -1,20 +1,33 @@
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
-manager = Manager(app)
+# manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index')) # Post/Redirect/Get pattern
     return render_template('index.html',
-                            current_time=datetime.utcnow())
+                            current_time=datetime.utcnow(),
+                            form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
