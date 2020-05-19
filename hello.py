a5656f8eab5afe7eea3c4c40c9e12134a0be29a9
Richard Khillah <RKhillah@ucla.edu>
2020-05-18 22:37:22 -0700

    flash messages

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -23,7 +23,11 @@ class NameForm(FlaskForm):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None or old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index')) # Post/Redirect/Get pattern
     return render_template('index.html',
                             current_time=datetime.utcnow(),
