9c3f831c5c9bbc1e9e241d27251f9cb0ab6d59fd
Richard Khillah <RKhillah@ucla.edu>
2020-05-19 15:34:00 -0700

    refactor models out

--- flasky.py
+++ flasky.py
@@ -24,25 +24,7 @@ class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Role %r>' % self.name
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
+from app.models import Role, User
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
