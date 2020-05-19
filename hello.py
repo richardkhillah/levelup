05031677d8bdb723f1734ba05bf0e941348d9646
Richard Khillah <RKhillah@ucla.edu>
2020-05-19 13:29:07 -0700

    send_async_email
    Dispatch background thread to send email so browser is not blocked upon user registration

--- hello.py
+++ hello.py
@@ -35,13 +35,20 @@ migrate = Migrate(app, db)
 mail = Mail(app)
 
 from flask_mail import Message
+from threading import Thread
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
                 sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
