c02330e03cfa32586f21dd99e9c63a989936169e
Richard Khillah <RKhillah@ucla.edu>
2020-05-19 22:11:02 -0700

    add basic unittests and cli command

--- flasky.py
+++ flasky.py
@@ -1,3 +1,5 @@
+import os
+import click
 from app import create_app, db
 from flask_migrate import Migrate
 
@@ -10,3 +12,14 @@ def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
 from app.models import Role, User
+
+@app.cli.command()
+@click.argument('test_names', nargs=-1)
+def test(test_names):
+    """Run the unit tests."""
+    import unittest
+    if test_names:
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
+    else:
+        tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
