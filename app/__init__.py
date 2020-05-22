2806fc2d2c566e91b83a4c104219f4a146acdae6
Richard Khillah <RKhillah@ucla.edu>
2020-05-22 15:58:44 -0700

    add login UserMixin and login_manager

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from flask_login import LoginManager
 
 from config import config
 
@@ -10,6 +11,9 @@ bootstrap = Bootstrap()
 moment = Moment()
 db = SQLAlchemy()
 mail = Mail()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong' # track user IP & browser agent
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -20,6 +24,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     mail.init_app(app)
+    login_manager.init_app(app)
 
     # register blueprints (views, errors, etc)
     from .main import main as main_blueprint
