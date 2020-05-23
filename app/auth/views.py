87d5cc2ad36797c4b4ffd9ed66ae0077c54822d4
Richard Khillah <RKhillah@ucla.edu>
2020-05-22 19:59:38 -0700

    new user registration

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required
 from . import auth
-from .forms import LoginForm
+from .. import db
+from .forms import LoginForm, RegistrationForm
 from ..models import User
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -21,3 +22,14 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
