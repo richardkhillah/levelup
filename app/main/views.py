15c312a1f5332254421f66d6dabe8ba38dca348c
Richard Khillah <RKhillah@ucla.edu>
2020-05-22 22:01:00 -0700

    implement full auth module

--- app/main/views.py
+++ app/main/views.py
@@ -9,26 +9,7 @@ from ..email import send_email
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-            if current_app.config['FLASKY_ADMIN']:
-                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
-                    'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('main.index')) # Post/Redirect/Get pattern
-    return render_template('index.html',
-                            current_time=datetime.utcnow(),
-                            form=form, name=session.get('name'),
-                            known=session.get('known', False))
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @main.route('/user/<name>')
 def user(name):
