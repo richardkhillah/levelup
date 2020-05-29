4cfb9e07775429981f945647183bdb55568fab78
Richard Khillah <RKhillah@ucla.edu>
2020-05-28 20:40:28 -0700

    Update __init__.py

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,6 +4,8 @@ main = Blueprint('main', __name__)
 
 from . import views, errors
 
+from ..models.models import Permission
+
 @main.app_context_processor
 def inject_permissions():
     return dict(Permission=Permission)
