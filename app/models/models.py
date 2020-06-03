9fc884f797b69de78bbc7e72171010d7e145a64a
Richard Khillah <RKhillah@ucla.edu>
2020-06-02 21:10:50 -0700

    Add Posts and appropiate migration script
    Local copy of postgres db was upgraded prior to git commit

--- app/models/models.py
+++ app/models/models.py
@@ -60,6 +60,8 @@ class User(UserMixin, db.Model):
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
     avatar_hash = db.Column(db.String(32))
 
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
         if self.role is None:
@@ -177,3 +179,11 @@ def load_user(user_id):
         or None
     """
     return User.query.get(int(user_id))
+
+
+class Post(db.Model):
+    __tablename__='posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
