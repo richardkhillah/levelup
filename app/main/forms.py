7e936429a8c60f79641dcf2ea8ed543e54036292
Richard Khillah <RKhillah@ucla.edu>
2020-06-04 21:24:46 -0700

    pagedown, markdown, bleach and html_body migration and upgrade

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextField
 from wtforms import SelectField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, Regexp
+from flask_pagedown.fields import PageDownField
 from ..models.models import Role
 
 class NameForm(FlaskForm):
@@ -46,5 +47,5 @@ class EditProfileAdminForm(FlaskForm):
             raise ValidationError('Username already in use.')
 
 class PostForm(FlaskForm):
-    body = TextField("What's on your mind?", validators=[DataRequired()])
+    body = PageDownField("What's on your mind?", validators=[DataRequired()])
     submit = SubmitField('Submit')
--- app/models/models.py
+++ app/models/models.py
@@ -6,6 +6,9 @@ from flask import current_app, request
 from datetime import datetime
 import hashlib
 
+from markdown import markdown
+import bleach
+
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
@@ -185,5 +188,17 @@ class Post(db.Model):
     __tablename__='posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue,initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True))
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
