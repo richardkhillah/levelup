2f6f6645b922718fe24a9d78f790faa8b9ea6ac0
Richard Khillah <RKhillah@ucla.edu>
2020-06-14 18:08:37 -0700

    test cases

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,7 +1,9 @@
 import unittest
 import time
+from datetime import datetime
 from app import create_app, db
-from app.models.models import User, AnonymousUser, Role, Permission
+from app.models import User, AnonymousUser, Role, Permission, Follow
+
 
 class UserModelTestCase(unittest.TestCase):
     def setUp(self):
@@ -63,79 +65,155 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password='cat')
         db.session.add(u)
         db.session.commit()
-        token = u.generate_reset_password_token()
+        token = u.generate_reset_token()
         self.assertTrue(User.reset_password(token, 'dog'))
         self.assertTrue(u.verify_password('dog'))
-        self.assertFalse(u.verify_password('cat'))
 
     def test_invalid_reset_token(self):
         u = User(password='cat')
         db.session.add(u)
         db.session.commit()
-        token = u.generate_reset_password_token()
-        self.assertFalse(User.reset_password(token + 'a', 'dog'))
+        token = u.generate_reset_token()
+        self.assertFalse(User.reset_password(token + 'a', 'horse'))
         self.assertTrue(u.verify_password('cat'))
 
     def test_valid_email_change_token(self):
-        u = User(email='drew@example.com', password='cat')
+        u = User(email='john@example.com', password='cat')
         db.session.add(u)
         db.session.commit()
-        token = u.generate_change_email_token('david@example.com')
+        token = u.generate_email_change_token('susan@example.org')
         self.assertTrue(u.change_email(token))
-        self.assertTrue(u.email == 'david@example.com')
+        self.assertTrue(u.email == 'susan@example.org')
 
     def test_invalid_email_change_token(self):
-        u1 = User(email='drew@example.com', password='cat')
-        u2 = User(email='susan@example.com', password='cat')
+        u1 = User(email='john@example.com', password='cat')
+        u2 = User(email='susan@example.org', password='dog')
         db.session.add(u1)
         db.session.add(u2)
         db.session.commit()
-        token = u1.generate_change_email_token('john@gmail.com')
+        token = u1.generate_email_change_token('david@example.net')
         self.assertFalse(u2.change_email(token))
-        self.assertTrue(u2.email == 'susan@example.com')
+        self.assertTrue(u2.email == 'susan@example.org')
 
     def test_duplicate_email_change_token(self):
-        u1 = User(email='drew@example.com', password='cat')
-        u2 = User(email='susan@example.com', password='cat')
+        u1 = User(email='john@example.com', password='cat')
+        u2 = User(email='susan@example.org', password='dog')
         db.session.add(u1)
         db.session.add(u2)
         db.session.commit()
-        token = u1.generate_change_email_token('susan@example.com')
-        self.assertFalse(u1.change_email(token))
-        self.assertTrue(u1.email == 'drew@example.com')
+        token = u2.generate_email_change_token('john@example.com')
+        self.assertFalse(u2.change_email(token))
+        self.assertTrue(u2.email == 'susan@example.org')
 
-    def test_user_roles(self):
-        u = User(email='john@example.com', password='dog')
+    def test_user_role(self):
+        u = User(email='john@example.com', password='cat')
         self.assertTrue(u.can(Permission.FOLLOW))
         self.assertTrue(u.can(Permission.COMMENT))
-        self.assertTrue(u.can(Permission.WRITE_ARTICLES))
-        self.assertFalse(u.can(Permission.MODERATE_COMMENTS))
-        self.assertFalse(u.can(Permission.ADMINISTER))
+        self.assertTrue(u.can(Permission.WRITE))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
 
     def test_moderator_role(self):
-        role = Role.query.filter_by(name='Moderator').first()
-        u = User(email='john@example.com', password='dog', role=role)
+        r = Role.query.filter_by(name='Moderator').first()
+        u = User(email='john@example.com', password='cat', role=r)
         self.assertTrue(u.can(Permission.FOLLOW))
         self.assertTrue(u.can(Permission.COMMENT))
-        self.assertTrue(u.can(Permission.WRITE_ARTICLES))
-        self.assertTrue(u.can(Permission.MODERATE_COMMENTS))
-        self.assertFalse(u.can(Permission.ADMINISTER))
+        self.assertTrue(u.can(Permission.WRITE))
+        self.assertTrue(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
 
     def test_administrator_role(self):
-        role = Role.query.filter_by(name='Administrator').first()
-        u = User(email='john@example.com', password='dog', role=role)
+        r = Role.query.filter_by(name='Administrator').first()
+        u = User(email='john@example.com', password='cat', role=r)
         self.assertTrue(u.can(Permission.FOLLOW))
         self.assertTrue(u.can(Permission.COMMENT))
-        self.assertTrue(u.can(Permission.WRITE_ARTICLES))
-        self.assertTrue(u.can(Permission.MODERATE_COMMENTS))
-        self.assertTrue(u.can(Permission.ADMINISTER))
-
-    def test_roles_and_permissions(self):
-        Role.insert_roles()
-        u = User(email='john@example.com', password='cat')
-        self.assertTrue(u.can(Permission.WRITE_ARTICLES))
-        self.assertFalse(u.can(Permission.MODERATE_COMMENTS))
+        self.assertTrue(u.can(Permission.WRITE))
+        self.assertTrue(u.can(Permission.MODERATE))
+        self.assertTrue(u.can(Permission.ADMIN))
 
     def test_anonymous_user(self):
         u = AnonymousUser()
         self.assertFalse(u.can(Permission.FOLLOW))
+        self.assertFalse(u.can(Permission.COMMENT))
+        self.assertFalse(u.can(Permission.WRITE))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
+
+    def test_timestamps(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        self.assertTrue(
+            (datetime.utcnow() - u.member_since).total_seconds() < 3)
+        self.assertTrue(
+            (datetime.utcnow() - u.last_seen).total_seconds() < 3)
+
+    def test_ping(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        time.sleep(2)
+        last_seen_before = u.last_seen
+        u.ping()
+        self.assertTrue(u.last_seen > last_seen_before)
+
+    def test_gravatar(self):
+        u = User(email='john@example.com', password='cat')
+        with self.app.test_request_context('/'):
+            gravatar = u.gravatar()
+            gravatar_256 = u.gravatar(size=256)
+            gravatar_pg = u.gravatar(rating='pg')
+            gravatar_retro = u.gravatar(default='retro')
+        self.assertTrue('https://secure.gravatar.com/avatar/' +
+                        'd4c74594d841139328695756648b6bd6'in gravatar)
+        self.assertTrue('s=256' in gravatar_256)
+        self.assertTrue('r=pg' in gravatar_pg)
+        self.assertTrue('d=retro' in gravatar_retro)
+
+    def test_follows(self):
+        u1 = User(email='john@example.com', password='cat')
+        u2 = User(email='susan@example.org', password='dog')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        self.assertFalse(u1.is_following(u2))
+        self.assertFalse(u1.is_followed_by(u2))
+        timestamp_before = datetime.utcnow()
+        u1.follow(u2)
+        db.session.add(u1)
+        db.session.commit()
+        timestamp_after = datetime.utcnow()
+        self.assertTrue(u1.is_following(u2))
+        self.assertFalse(u1.is_followed_by(u2))
+        self.assertTrue(u2.is_followed_by(u1))
+        self.assertTrue(u1.followed.count() == 2)
+        self.assertTrue(u2.followers.count() == 2)
+        f = u1.followed.all()[-1]
+        self.assertTrue(f.followed == u2)
+        self.assertTrue(timestamp_before <= f.timestamp <= timestamp_after)
+        f = u2.followers.all()[-1]
+        self.assertTrue(f.follower == u1)
+        u1.unfollow(u2)
+        db.session.add(u1)
+        db.session.commit()
+        self.assertTrue(u1.followed.count() == 1)
+        self.assertTrue(u2.followers.count() == 1)
+        self.assertTrue(Follow.query.count() == 2)
+        u2.follow(u1)
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        db.session.delete(u2)
+        db.session.commit()
+        self.assertTrue(Follow.query.count() == 1)
+
+    def test_to_json(self):
+        u = User(email='john@example.com', password='cat')
+        db.session.add(u)
+        db.session.commit()
+        with self.app.test_request_context('/'):
+            json_user = u.to_json()
+        expected_keys = ['url', 'username', 'member_since', 'last_seen',
+                         'posts_url', 'followed_posts_url', 'post_count']
+        self.assertEqual(sorted(json_user.keys()), sorted(expected_keys))
+        self.assertEqual('/api/v1/users/' + str(u.id), json_user['url'])
