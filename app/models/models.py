eb1122a85ad91924642239a0e52fd2ba29d05ba8
Richard Khillah <RKhillah@ucla.edu>
2020-06-15 13:47:01 -0700

    add a db.session.commit() to all adds
    I do not like COMMIT_ON_TEARDOWN. It leaves too much ambiguity in the code. db.session.commit() while seeminly redundant is explicitly better.

--- app/models/models.py
+++ app/models/models.py
@@ -109,7 +109,7 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(default=True).first()
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = self.gravatar_hash()
-        self.follow(self)
+        # self.follow(self)
 
     @property
     def password(self):
