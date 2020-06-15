3dca9f68bc3884d6c28c435e3a97d230d1b90e2d
Richard Khillah <RKhillah@ucla.edu>
2020-06-14 23:16:40 -0700

    add HerokuConfig to config dict

--- config.py
+++ config.py
@@ -84,6 +84,7 @@ config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
+    'heroku': HerokuConfig,
 
     'default': DevelopmentConfig
 }
