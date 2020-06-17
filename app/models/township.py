0f35e7e92a815ddeff94d6a097bfe7d6e832dbd5
Richard Khillah <RKhillah@ucla.edu>
2020-06-16 20:47:40 -0700

    update fake and comment out bind_key

--- app/models/township.py
+++ app/models/township.py
@@ -3,12 +3,14 @@ from sqlalchemy import func
 
 class BaseA(db.Model):
     __abstract__ = True
+    # __bind_key__ = 'township'
     name = db.Column(db.String(64))
     required_level = db.Column(db.Integer)
     time_to_make = db.Column(db.String(16))
 
 class BaseB(db.Model):
     __abstract__ = True
+    # __bind_key__ = 'township'
     total_quantity = db.Column(db.Integer, default=1)
     # TODO: quantity_built is a property of the town - refactor.
     quantity_built = db.Column(db.Integer, default=0)
@@ -17,6 +19,7 @@ class BaseB(db.Model):
 
 class Source(BaseA, BaseB):
     __tablename__ = 'source'
+    # __bind_key__ = 'township'
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(50))
     items = db.relationship('Item', back_populates='source')
@@ -27,36 +30,43 @@ class Source(BaseA, BaseB):
 
 class Farming(Source):
     __tablename__ = 'farming'
+    # __bind_key__ = 'township'
     id = db.Column(db.Integer, db.ForeignKey('source.id'), primary_key=True)
     __mapper_args__ = {'polymorphic_identity': 'farming'}
 
 class Factory(Source):
     __tablename__ = 'factory'
+    # __bind_key__ = 'township'
     id = db.Column(db.Integer, db.ForeignKey('source.id'), primary_key=True)
     __mapper_args__ = {'polymorphic_identity': 'factory'}
 
 class Island(Source):
     __tablename__ = 'island'
+    # __bind_key__ = 'township'
     id = db.Column(db.Integer, db.ForeignKey('source.id'), primary_key=True)
     __mapper_args__ = {'polymorphic_identity': 'island'}
 
 class Locomotive(Source):
     __tablename__ = 'locomotive'
+    # __bind_key__ = 'township'
     id = db.Column(db.Integer, db.ForeignKey('source.id'), primary_key=True)
     __mapper_args__ = {'polymorphic_identity': 'locomotive'}
 
 class House(Source):
     __tablename__ = 'house'
+    # __bind_key__ = 'township'
     id = db.Column(db.Integer, db.ForeignKey('source.id'), primary_key=True)
     __mapper_args__ = {'polymorphic_identity': 'house'}
 
 class CommunityBuilding(Source):
     __tablename__ = 'community_building'
+    # __bind_key__ = 'township'
     id = db.Column(db.Integer, db.ForeignKey('source.id'), primary_key=True)
     __mapper_args__ = {'polymorphic_identity': 'community_building'}
 
 class Special(Source):
     __tablename__ = 'special'
+    # __bind_key__ = 'township'
     id = db.Column(db.Integer, db.ForeignKey('source.id'), primary_key=True)
     __mapper_args__ = {'polymorphic_identity': 'special'}
 
@@ -74,6 +84,7 @@ source = {
 
 class Item(BaseA):
     __tablename__ = 'item'
+    # __bind_key__ = 'township'
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(32))
     source_id = db.Column(db.ForeignKey('source.id'))
@@ -84,31 +95,37 @@ class Item(BaseA):
 
 class Plant(Item):
     __tablename__ = 'plant'
+    # __bind_key__ = 'township'
     id = db.Column(db.Integer, db.ForeignKey(Item.id), primary_key=True)
     __mapper_args__ = {'polymorphic_identity': 'plant'}
 
 class Manufactured(Item):
     __tablename__ = 'manufactured'
+    # __bind_key__ = 'township'
     id = db.Column(db.Integer, db.ForeignKey(Item.id), primary_key=True)
     __mapper_args__ = {'polymorphic_identity': 'manufactured'}
 
 class Imported(Item):
     __tablename__ = 'imported'
+    # __bind_key__ = 'township'
     id = db.Column(db.Integer, db.ForeignKey(Item.id), primary_key=True)
     __mapper_args__ = {'polymorphic_identity': 'imported'}
 
 class Material(Item):
     __tablename__ = 'material'
+    # __bind_key__ = 'township'
     id = db.Column(db.Integer, db.ForeignKey(Item.id), primary_key=True)
     __mapper_args__ = {'polymorphic_identity': 'material'}
 
 class Foundry(Item):
     __tablename__ = 'foundry'
+    # __bind_key__ = 'township'
     id = db.Column(db.Integer, db.ForeignKey(Item.id), primary_key=True)
     __mapper_args__ = {'polymorphic_identity': 'foundry'}
 
 # class Ore(Item):
 #     __tablename__ = 'ore'
+# __bind_key__ = 'township'
 #     id = db.Column(db.Integer, db.ForeignKey(Item.id), primary_key=True)
 #     __mapper_args__ = {
 #         'polymorphic_identity': 'ore'
@@ -116,6 +133,7 @@ class Foundry(Item):
 
 # class Gem(Item):
 #     __tablename__ = 'gem'
+# __bind_key__ = 'township'
 #     id = db.Column(db.Integer, db.ForeignKey(Item.id), primary_key=True)
 #     __mapper_args__ = {
 #         'polymorphic_identity': 'gem'
@@ -135,6 +153,7 @@ item = {
 from .models import User
 class Town(db.Model):
     __tablename__ = 'town'
+    # __bind_key__ = 'township'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     level = db.Column(db.Integer, default=1)
